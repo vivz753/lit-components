46724f0580038d4079f0f6798f65258c76419bfc
Vivian Leung <vivian.leung@infor.com>
2021-09-29 17:42:18 -0400

    add rollup plugin for live reload

--- lit-tailwind/rollup.config.js
+++ lit-tailwind/rollup.config.js
@@ -1,36 +1,38 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import typescript from '@rollup/plugin-typescript';
-import { babel } from '@rollup/plugin-babel';
-import postcss from 'rollup-plugin-postcss';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import { babel } from "@rollup/plugin-babel";
+import postcss from "rollup-plugin-postcss";
+import livereload from "rollup-plugin-livereload";
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.ts',
-	output: {
-		file: 'public/bundle.js',
-		format: 'es',
-		sourcemap: true
-	},
+  input: "src/main.ts",
+  output: {
+    file: "public/bundle.js",
+    format: "es",
+    sourcemap: true,
+  },
   watch: {
     chokidar: true,
-    include: 'src/**'
+    include: "src/**",
   },
-	plugins: [
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
+  plugins: [
+    livereload("public"),
+    resolve(), // tells Rollup how to find date-fns in node_modules
+    commonjs(), // converts date-fns to ES modules
     postcss(),
     typescript({
-      include: ['src/**/*.ts']  
+      include: ["src/**/*.ts"],
     }),
     babel({
-      babelHelpers: 'bundled',
-      exclude: "node_modules/**"
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
     }),
-		production && terser({ format: {comments: false } }) // minify, but only in production
-	]
+    production && terser({ format: { comments: false } }), // minify, but only in production
+  ],
 };
