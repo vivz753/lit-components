947d2d9f6743be54603d61150ec28e3da361fde9
Vivian Leung <vivian.leung@infor.com>
2021-09-29 17:43:13 -0400

    working ids-input with clearable, disabled, readonly, label, placeholder attributes and styles

--- lit-tailwind/src/components/ids-input/ids-input.ts
+++ lit-tailwind/src/components/ids-input/ids-input.ts
@@ -3,43 +3,95 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+const defaultColor = `text-pink-300 placeholder-pink-200`;
+const labelStyle = `text-lg mx-2 block`;
+const inputStyle = `text-base block relative rounded-sm h-8 p-2 focus:outline-none`;
+
 @customElement("ids-input")
 class IdsInput extends LitElement {
   @property() // properties are type String by default
-  text?: string = "Test";
+  label?: string = "Default Label";
 
-  @property()
-  color?: string;
+  @property() // properties are type String by default
+  text?: string;
 
-  @property({
-    reflect: true,
-  })
-  value?: string;
+  // @property()
+  // color?: string;
+
+  @property({ reflect: true })
+  value?: string = "Default Initial Value";
+
+  @property({ reflect: true })
+  placeholder?: string = "Default Placeholder Text";
+
+  @property({ type: Boolean })
+  readOnly?: boolean = false;
+
+  @property({ type: Boolean, reflect: true })
+  disabled?: boolean = false;
+
+  @property({ type: Boolean, reflect: true })
+  clearable?: boolean = true;
 
   createRenderRoot() {
     return this;
   }
 
-  updateValue(e: {target: HTMLInputElement}) {
-    console.log(e.target!.value)
+  updateValue(e: { target: HTMLInputElement }) {
     this.value = e.target!.value;
+  }
+
+  clearValue() {
+    this.value = "";
   }
 
   render() {
     return html`
-      <input
-        type="text"
-        class="rounded
-        w-full
-        mx-auto
-        shadow-lg
-        p-2
-        "
-        value="${this.value}"
-        @change="${this.updateValue}"
-        @input="${this.updateValue}"
-      >
-      </input>
+      <div class="flex flex-col items-start border-1 border-black justify-start w-full">
+        <label class="
+          ${labelStyle}
+          ${this.readOnly || this.disabled ? "text-gray-400" : defaultColor} 
+        ">
+          ${this.label}
+        </label>
+        <div 
+          class="flex flex-row mx-2 relative rounded-sm
+            ${this.clearable && "border-red-500 bg-white"}
+        ">
+          <input
+            type="text"
+            class="
+              ${inputStyle}
+              ${this.clearable ? "mr-8" : "focus:ring-2 focus:ring-pink-400"}
+              ${
+                this.readOnly || this.disabled
+                  ? "bg-gray-300 text-white"
+                  : defaultColor
+              }
+            "
+            .placeholder="${this.placeholder}"
+            .value="${this.value}"
+            .disabled="${this.disabled || this.readOnly}"
+            @change="${this.updateValue}"
+            @input="${this.updateValue}"
+          >
+          </input>
+          ${
+            this.value && this.clearable
+              ? html`<div
+                  class="absolute right-0 mr-1 h-8 w-8 flex justify-center align-items"
+                >
+                  <span
+                    @click="${this.clearValue}"
+                    class="text-center text-red-500 cursor-pointer rounded-full my-auto bg-pink-100 bg-opacity-50 hover:bg-red-500 hover:text-pink-100 h-6 w-6"
+                  >
+                    x
+                  </span>
+                </div>`
+              : ""
+          }
+        </div>
+      </div>
     `;
   }
 }
--- lit-tailwind/src/main.ts
+++ lit-tailwind/src/main.ts
@@ -14,9 +14,9 @@ export default class AppComponent extends LitElement {
   render() {
     return html`
       <div class="bg-yellow-100 h-screen w-full">
-      <h1 class="mx-auto my-4 py-4 text-center shadow-lg text-xl w-1/2 bg-red-500">IDS Components</h1>
-      <ids-card text="hello" color="blue"></ids-card>
-      <ids-input value="sh"></ids-input>
+        <h1 class="mx-auto my-4 py-4 text-center shadow-lg text-xl w-1/2 bg-red-500">IDS Components</h1>
+        <ids-card text="hello" color="blue"></ids-card>
+        <ids-input label="Pokedex" placeholder="Type any Pokemon..." value=""></ids-input>
       </div>
     `;
   }
